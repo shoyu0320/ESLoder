b7c85b83113d77c43bc6e6766edb1936889b15eb
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-11-04 18:35:52 +0900

    DBを変更しました

--- excel/excel/settings.py
+++ excel/excel/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "excel.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "exceldb",
+        "USER": "shoyu",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
