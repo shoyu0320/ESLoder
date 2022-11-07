353ff535260e4a354bc9566ac70234d64f8cf377
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-11-07 18:28:14 +0900

    upload_excelをurlで反映されるようにした

--- excel/excel/urls.py
+++ excel/excel/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from upload_excel import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.UploadExcelView.as_view(), name="index"),
+    path("excel_sheet/", include("upload_excel.urls")),
 ]
