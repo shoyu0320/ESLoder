ab2702247b3fde89de07f135094bab68c06c5d17
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-11-11 15:06:21 +0900

    urlを書き換え

--- excel/upload_excel/urls.py
+++ excel/upload_excel/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
         "user=?<str:user_id>?",
         views.UploadExcelView.as_view(), name="upload"
     ),
+    path(
+        "user=?<str:user_id>?/cell=?<int:cell_id>@<str:cell_uuid>?",
+        views.CellUpdateView.as_view(), name="update"
+    ),
 ]
